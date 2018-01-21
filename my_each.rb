14b35f665545576c06674777716a1cdec8602a4c
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-21 08:38:54 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    index = 0
+    while index < array.length
+      yield array[index]
+      index += 1
+    end
+  end
+  return array
 end
